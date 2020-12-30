eea288e112514d52e332c75560d40bf0b4f6f26a
Xenonimous <rian.kurniawan1018@students.unila.ac.id>
2020-12-30 14:44:30 +0700

    CORS unblocked

--- src/components/GetPenduduk.js
+++ src/components/GetPenduduk.js
@@ -8,7 +8,7 @@ export default class GetPenduduk extends Component {
     }
 
     async componentDidMount() {
-        await axios.get('https://www.bps.go.id/indikator/indikator/download_json/1800/api_pub/50/da_03/1'
+        await axios.get('https://www.bps.go.id/indikator/indikator/download_json/1800/api_pub/50/da_03/1', {headers: {"Access-Control-Allow-Origin": "*"}},
         )
             .then( response => {
                 this.setState({ temporary: response.data.data[13].penduduk_jumlah_penduduk })
