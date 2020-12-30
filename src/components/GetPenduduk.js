b4176f69e05c57a2de9733d2d41dd2d76a24e292
Xenonimous <rian.kurniawan1018@students.unila.ac.id>
2020-12-30 14:53:29 +0700

    cors

--- src/components/GetPenduduk.js
+++ src/components/GetPenduduk.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+var url = 'https://www.bps.go.id/indikator/indikator/download_json/1800/api_pub/50/da_03/1',
 export default class GetPenduduk extends Component {
 
     state = {
@@ -8,7 +9,7 @@ export default class GetPenduduk extends Component {
     }
 
     async componentDidMount() {
-        await axios.get('https://www.bps.go.id/indikator/indikator/download_json/1800/api_pub/50/da_03/1', {headers: {"Access-Control-Allow-Origin": "*"}},
+        await axios.get( url, {headers: {"Access-Control-Allow-Origin": "*"}},
         )
             .then( response => {
                 this.setState({ temporary: response.data.data[13].penduduk_jumlah_penduduk })
